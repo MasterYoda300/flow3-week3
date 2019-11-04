9a9ec6cc5f718dbf41b41c83565174431b8a982b
artin <artin-4000@hotmail.com>
2019-11-04 21:55:41 +0100

    part 2 start

---
+++ mandag/reactrouter/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom"
+import data from "./data/data.json"
+
+
+<Router>
+<Switch>
+//Add Your Routes here
+  </Switch>
+</Router>
+
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
